b19ba8bdf0c14962dd4b0d88f945259bd60d9206
Anas Asimi <anas2020cr7@gmail.com>
2023-12-23 21:34:23 +0100

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+
+/**
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @n1: the first node to swap.
+ * @n2: The second node to swap.
+ */
+void swap_nodes(listint_t **list, listint_t *n1, listint_t *n2)
+{
+	n1->next = n2->next;
+	n2->prev = n1->prev;
+
+	n1->prev = n2;
+	n2->next = n1;
+
+	if (n2->prev == NULL)
+		*list = n2;
+	else
+		n2->prev->next = n2;
+
+	if (n1->next != NULL)
+		n1->next->prev = n1;
+}
+
+/**
+ * insertion_sort_list - Entry point
+ * @list : list
+ * Return: void
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *tmp;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	for (current = *list; current != NULL; current = tmp)
+	{
+		tmp = current->next;
+		if (current->next != NULL && current->n > current->next->n)
+		{
+			swap_nodes(list, current, current->next);
+			print_list((const listint_t *)*list);
+			tmp = *list;
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
