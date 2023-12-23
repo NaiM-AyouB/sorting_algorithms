1f7ae841c9cafafad6d5e03d77a3cb7e6cc58d25
Anas Asimi <anas2020cr7@gmail.com>
2023-12-24 00:55:49 +0100

    fix task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,26 @@
 #include "sort.h"
 /**
  * bubble_sort - sort the array elements from min to max
- * @array: array to sort 
+ * @array: array to sort
  * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
 
-	size_t i, index, tmp = 0;
+	size_t i, j, tmp = 0;
 
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
+
 	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+		for (j = 0; j < size; j++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if ((j + 1) < size && array[j] > array[j + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
 			}
 		}
 }
